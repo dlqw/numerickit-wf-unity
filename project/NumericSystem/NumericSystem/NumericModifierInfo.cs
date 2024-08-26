5d41a0ae03d50bf66000ba7d208310a2e260c549
quxinyue <2900814100@qq.com>
2024-08-26 15:58:56 +0800

    测试action

---
+++ project/NumericSystem/NumericSystem/NumericModifierInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace WFramework.CoreGameDevKit.NumericSystem
+{
+    public record NumericModifierInfo
+    {
+        public readonly string[] Tags;
+        public readonly string   Name;
+
+        public int Count;
+
+        public NumericModifierInfo(string[] tags, string name, int count)
+        {
+            Tags  = tags;
+            Name  = name;
+            Count = count;
+        }
+    }
+}
