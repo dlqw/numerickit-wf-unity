fee5ec4de9a3ffe2a7ed11a875509a899d0b134e
rdququ <113582955+dlqw@users.noreply.github.com>
2026-02-03 23:26:35 +0800

    Remove debug and temporary files

--- test_fixed.cs
+++
@@ -1,6 +0,0 @@
-using WFramework.CoreGameDevKit.NumericSystem;
-
-var numeric = new Numeric(100);
-Console.WriteLine($"FinalValue: {numeric.FinalValue}");
-Console.WriteLine($"FinalValueF: {numeric.FinalValueF}");
-Console.WriteLine($"Origin: {numeric.GetOriginValue()}");
